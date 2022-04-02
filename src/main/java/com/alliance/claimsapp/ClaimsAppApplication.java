d88028774c26842a656d2fdd352f47dbbd364650
Jhudel D. Belandres <71002835+Jhudel4277@users.noreply.github.com>
2022-04-02 14:01:13 +0800

    Add files via upload

---
+++ src/main/java/com/alliance/claimsapp/ClaimsAppApplication.java
@@ -0,0 +1,13 @@
+package com.alliance.claimsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alliance/claimsapp/ClaimsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliance.claimsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
